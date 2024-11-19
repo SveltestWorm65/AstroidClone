e18a6a4df1b6ec4a35708ec4a03527a8d7a65961
SveltestWorm65 <wormgamedevelopment@gmail.com>
2024-11-19 10:13:41 -0800

    Iteration 2
    Working on Better controls
    going to get shooting in
    got screen wrapping in
    going to get asteroids in eventually
    working on to get better drag

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Variables")]
-    private float horizontalInput;
     [SerializeField] private float turnSpeed;
+    //forward & back
     private float verticalInput;
+    //rotation
+    private float clockWiseInput;
+    private float counterClockWiseInput;
 
     [Header("Components")]
     public Rigidbody rb;
 
-    [Header("Boosting")]
+    [Header("Speed")]
     public float thrustForce;
+
+    [Header("GameObjects")]
+    public GameObject playerProjectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +29,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+        //getting the better rotation controls with controller and keyboard
+        if (Input.GetButton("Clockwise"))
+        {
+            transform.Rotate(Vector3.up * -1 * turnSpeed * Time.deltaTime);
+        }
 
+        if (Input.GetButton("CounterClockwise"))
+        {
+            transform.Rotate(Vector3.down * -1 * turnSpeed * Time.deltaTime);
+        }
+
+        //shooting 
+        if(Input.GetButtonDown("Shoot"))
+        {
+            Instantiate(playerProjectilePrefab);
+        }
     }
     private void FixedUpdate()
     {
+        //forward and backwards movement
         verticalInput = Input.GetAxis("Vertical");
         rb.AddRelativeForce(Vector3.forward * verticalInput * thrustForce);
+
+        
+       
     }
 }
--- Assets/Scripts/ScreenWrapper.cs
+++ Assets/Scripts/ScreenWrapper.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class ScreenWrapper : MonoBehaviour
 {
-    public GameObject player;
-
-    public GameObject leftWall;
-    public GameObject rightWall;
-    public GameObject bottomWall;
-    public GameObject topWall;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [Header("Limits")]
+    public float xMax;
+    public float xMin;
+    public float zMax;
+    public float zMin;
+  
     // Update is called once per frame
     void Update()
     {
-        
+        //making the objects go to the other side of the x axis
+        if(transform.position.x > xMax)
+        {
+            transform.position = new Vector3(
+                xMin,
+                transform.position.y,
+                transform.position.z
+                ); 
+        }
+        if (transform.position.x < xMin)
+        {
+            transform.position = new Vector3(
+                xMax,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+
+        //making the z axis do the same as x
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                zMin
+                );
+        }
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                zMax
+                );
+        }
     }
 }
